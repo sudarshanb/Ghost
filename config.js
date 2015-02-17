4cfd71730ed68f2ecb5a751ba59aecd31b3dcc2e
Sudarshan Bhalerao <sudarshanbhalerao@gmail.com>
2015-02-16 21:42:20 -0600

    Ignoring config file from deployment

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://tryghost.azurewebsites.net',
+        url: '<YOUR WEBSITE URL>',
         mail: {},
         database: {
             client: 'sqlite3',
